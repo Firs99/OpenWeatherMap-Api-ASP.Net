e761b39977f224679be300d9d3e029782961eaa7
Firs99 <slavytich1234@gmail.com>
2019-11-03 21:11:57 +0200

    Complited

---
+++ WebAppWeather/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System.Web.Mvc;
+using WebAppWeather.Models;
+
+namespace WebAppWeather.Controllers
+{
+    public class HomeController : Controller
+    {
+       [HttpGet]
+        public ActionResult Index()
+        {
+            return View(new WeatherHelper());
+        }
+        /// <summary>
+        /// Метод для обработки входящего параметра и отправки ответа с Openweatherapi.
+        /// </summary>
+        /// <param name="InputCity"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public ActionResult GetWeatherForecast(string InputCity)
+        {
+            WeatherHelper weather = new WeatherHelper();
+            weather.InputCity = InputCity;
+            return Json(weather.GetWeatherList(), JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ WebAppWeather/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebAppWeather.MvcApplication" Language="C#" %>
